b5dbd545e3c9766ba9b4deba09015e002200120c
KazuruK <esinnikita.2000@mail.ru>
2019-03-22 11:46:52 +0300

    Polynom_0.2 Исправления

--- src/test/java/PolynomialTest.java
+++ src/test/java/PolynomialTest.java
@@ -1,12 +1,15 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PolynomialTest {
 
     private Polynomial p1;
     private Polynomial p2;
 
-    private void initialization() {
+    @Before
+    public void initialization() {
         int[] array1 = new int[]{3, 2, 4, 0};
         p1 = new Polynomial(array1); // p1 = 4x^2+2x+3
         int[] array2 = new int[]{0, -1};
@@ -15,42 +18,43 @@ public class PolynomialTest {
 
     @Test
     public void sumTest() {
-        initialization();
         Polynomial result = p1.summarize(p2);
         Assert.assertEquals("4x^2+x+3", result.toString());
     }
 
     @Test
     public void subTest() {
-        initialization();
         Polynomial result = p1.subtract(p2);
         Assert.assertEquals("4x^2+3x+3", result.toString());
     }
 
     @Test
     public void multTest() {
-        initialization();
         Polynomial result = p1.multiply(p2);
         Assert.assertEquals("-4x^3-2x^2-3x", result.toString());
+        Polynomial p3 = new Polynomial(new int[]{1});
+        result = p1.multiply(p3);
+        Assert.assertEquals("4x^2+2x+3",result.toString());
     }
 
     @Test
     public void divTest() {
-        initialization();
-        Polynomial result = p1.divide(p2);
+        Polynomial result;
+        result = p1.divide(p2);
         Assert.assertEquals("-4x-2", result.toString());
+        Polynomial p3 = new Polynomial(new int[]{0});
+
+        assertThrows(IllegalArgumentException.class , () -> p1.divide(p3));
     }
 
     @Test
     public void remainderTest() {
-        initialization();
         Polynomial result = p1.getRemainder(p2);
         Assert.assertEquals("3", result.toString());
     }
 
     @Test
     public void equalsTest(){
-        initialization();
         Polynomial p3 = p1;
         Assert.assertFalse(p1.equals(p2));
         Assert.assertTrue(p1.equals(p3));
